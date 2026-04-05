df48cab224a86e18b06f2e214aaed403ba66b6d0
tanmay-mevada <tanmaymevada24@gmail.com>
2026-04-05 10:30:03 +0530

    shifting on cloud #1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate // Added Navigate for the catch-all route
 } from "react-router-dom";
 
 import Home from "./Pages/Home";
@@ -11,19 +12,23 @@ import Layout from "./Components/Layout";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 
+// We will create this admin component in the next step!
+import AdminLogin from "./Pages/AdminLogin"; 
+
 function App() {
   useEffect(() => {
-  const hasVisited = sessionStorage.getItem("hasVisited");
+    const hasVisited = sessionStorage.getItem("hasVisited");
 
-  if (!hasVisited) {
-    sessionStorage.setItem("hasVisited", "true");
-    window.location.reload();
-  }
-}, []);
+    if (!hasVisited) {
+      sessionStorage.setItem("hasVisited", "true");
+      window.location.reload();
+    }
+  }, []);
 
   return (
     <Router>
       <Routes>
+        {/* PUBLIC ROUTES: Wrapped in your standard Layout (Navbar, Footer, etc.) */}
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
@@ -31,6 +36,12 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Route>
+
+        {/* ADMIN ROUTE: Outside the layout so it has a completely separate, clean UI */}
+        <Route path="/admin" element={<AdminLogin />} />
+
+        {/* CATCH-ALL: If someone types a broken URL, send them safely home */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
